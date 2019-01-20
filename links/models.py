57522288eec7c7dad02f8d151101c4a4189c1378
Denizz <deniz1983@mail.ru>
2019-01-20 12:39:50 +0300

    Initialize repo

---
+++ links/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Link(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=1000)
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def publish(self):
+        self.published_date = created_date
+        self.save()
+
+    def __str__(self):
+        return self.title
